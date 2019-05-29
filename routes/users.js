b3ebc6d350b94a33ab65ce472739c14546a01d2b
279344809@qq.com <279344809@qq.com>
2019-05-29 16:33:11 +0800

    fix bugs

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,43 @@
 const router = require('koa-router')()
 
-const  sqlServices  = require('../dbcontrollers/mysqlconfig.js')
+const sqlServices = require('../dbcontrollers/mysqlconfig.js')
 
-router.prefix('/users')
+router.prefix('/zyf/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+// router.get('/', function (ctx, next) {
+//   ctx.body = 'this is a users response!'
+// })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+// router.get('/bar', function (ctx, next) {
+//   ctx.body = 'this is a users/bar response'
+// })
 
-router.post('/getinfo',async function (ctx, next) {
-
-  
+router.post('/getinfo', async function (ctx, next) {
   let dataList = await sqlServices.findAllUserData()
-  ctx.body = dataList
+  ctx.body = { code: 20000, data: dataList }
 })
+
+
+router.post('/login', async function (ctx, next) {
+  // console.log('111111')
+
+  let data = ctx.request.body;
+  // console.log(data)
+
+  let dataList = await sqlServices.findUserData(data.username)
+
+  if (dataList.length <= 0) {
+    ctx.body = { code: 50000, message: '用户不存在' }
+  } else {
+    if (dataList[0].password != data.password) {
+      ctx.body = { code: 50000, message: '密码错误' }
+    } else {
+      ctx.body = { code: 20000, data: dataList }
+    }
+
+
+  }
+})
+
 
 module.exports = router
