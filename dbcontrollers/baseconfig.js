e4e740356279923dfb96b00ac397475a696af7c0
279344809 <279344809@qq.com>
2019-08-23 21:00:46 +0800

    fix mysql config

--- dbcontrollers/baseconfig.js
+++ dbcontrollers/baseconfig.js
@@ -1,10 +1,21 @@
+// const config = {
+//     database: {
+//         DATABASE: 'zyf',
+//         USERNAME: 'root',
+//         PASSWORD: 'zhuyifei',
+//         PORT: '3306',
+//         HOST: '172.30.0.17'
+//     }
+// }
+// module.exports = config
+
 const config = {
     database: {
         DATABASE: 'zyf',
         USERNAME: 'root',
         PASSWORD: 'zhuyifei',
         PORT: '3306',
-        HOST: '172.30.0.17'
+        HOST: '127.0.0.1'
     }
 }
 module.exports = config
